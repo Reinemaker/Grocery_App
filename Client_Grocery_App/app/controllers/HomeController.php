0003e3c34feb9e471e77b56f1d4158435aa14810
NatalieMulodjanov <ntaliemulodjanov@gmail.com>
2022-04-01 17:11:23 -0400

    .

--- Client_Grocery_App/app/controllers/HomeController.php
+++ Client_Grocery_App/app/controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php 
 namespace app\controllers;
 
+// This client controller is an exapple of how to use controllers in client. Base yourselves on this.
 class HomeController extends \app\core\Controller {
 
     public function get($id=null){
