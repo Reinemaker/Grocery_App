2dfb328695f8fd5f3a18153ec5b68044dbb9f452
NatalieMulodjanov <ntaliemulodjanov@gmail.com>
2022-04-18 17:08:44 -0400

    fixed merge conflicts

--- Client_Grocery_App/app/controllers/ShoppingController.php
+++ Client_Grocery_App/app/controllers/ShoppingController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 // This client controller is an exapple of how to use controllers in client. Base yourselves on this.
 class ShoppingController extends \app\core\Controller {
 
-    public function get($id=null){
-        return $this->view("index", array("id" => $id));
-    }
+    // public function get($id=null){
+    //     return $this->view("index", array("id" => $id));
+    // }
 }
