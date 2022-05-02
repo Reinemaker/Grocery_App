2ffcd0617e00c4a3a1575402d310960d2290da01
Minjae Lee <1793875@edu.vaniercollege.qc.ca>
2022-05-02 18:44:10 -0400

    Adding hashing password on update account

--- Client_Grocery_App/app/views/Main/navbar.php
+++ Client_Grocery_App/app/views/Main/navbar.php
@@ -3,5 +3,6 @@
     <a href='<?= BASE ?>Payment/index'>Payment</a>
     <a href='<?= BASE ?>ShoppingCart/index'>Shopping Cart</a>
     <a href='<?= BASE ?>Transaction/index'>Transaction</a>
+    <a href='<?= BASE ?>Main/update'>Update Account</a>
     <a href='<?= BASE ?>Account/logout'>Logout</a>
 </nav>
--- Client_Grocery_App/app/views/Main/update.php
+++ Client_Grocery_App/app/views/Main/update.php
@@ -7,17 +7,14 @@
 </head>
 
 <body>
-
 	<form method="post" action='/Grocery_App/Client_Grocery_App/Account/updateAccount'>
-
-	<input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="First name" type="text" name="first_name" required/>
-    <input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Last name" type="text" name="last_name" pattern="^[A-Za-z]*$" title="Last name must only contain letters."/>
-    <input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Username" type="text" name="username" />
-    <input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Address" type="text" name="address" />
-    <input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Password" type="password" name="password" />
-    <input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Password confirmation:" type="password" name="password_confirm" />
-    <input style="text-align:center; margin: 0 auto; padding-right: 30px; padding-left: 30px" class="btn btn-success d-flex justify-content-center" type="submit" name="action" value="Register" />
-	
+		<input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="First name" type="text" name="first_name" required/>
+		<input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Last name" type="text" name="last_name" pattern="^[A-Za-z]*$" title="Last name must only contain letters."/>
+		<input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Username" type="text" name="username" />
+		<input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Address" type="text" name="address" />
+		<input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Password" type="password" name="password" />
+		<input class="form-control" style="width: 250px; margin-right: 10px; margin-bottom: 10px" placeholder="Password confirmation:" type="password" name="password_confirm" />
+		<input style="text-align:center; margin: 0 auto; padding-right: 30px; padding-left: 30px" class="btn btn-success d-flex justify-content-center" type="submit" name="action" value="Register" />
 	</form>
 </body>
 
