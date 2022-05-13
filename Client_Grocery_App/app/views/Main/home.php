0a83ef1906bb597aea01997b9d52958dffc9c139
Jonathan Euzebe <60105810+Jo152@users.noreply.github.com>
2022-05-12 23:06:30 -0400

    updated home

--- Client_Grocery_App/app/views/Main/home.php
+++ Client_Grocery_App/app/views/Main/home.php
@@ -3,21 +3,46 @@
 <head>
     <title>home page</title>
     <style>
-        .product {
-            width: 300px;
-        }
-        .product:hover {
-            background: lightgray;
-        }
-    </style>
 
-    <?php include('C:\xampp\htdocs\Grocery_App\Client_Grocery_App\app\views\Main\navbar.php'); ?>
+    .product {
+        width: 300px;
+    }
+    .product:hover {
+        background: lightgray;
+    }
+    footer {
+      background-color: #B1B3B4;
+      padding: 10px;
+      text-align: center;
+      color: white;
+      width: auto;
+  }
+  body{
+
+  }
+  .container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  background-color: #2196F3;
+  padding: 10px;
+}
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+
+</style>
+
+<?php include('C:\xampp\htdocs\Grocery_App\Client_Grocery_App\app\views\Main\navbar.php'); ?>
 </head>
 
 <body>
-    
 
-    <?php include('C:\xampp\htdocs\Grocery_App\Client_Grocery_App\app\views\Product\search.php'); ?>
+
+    <!--<?php include('C:\xampp\htdocs\Grocery_App\Client_Grocery_App\app\views\Product\search.php'); ?>
     <?php
     $baseUrl = BASE . "/Product/productDetails";
     if (isset($data['products'])) {
@@ -30,7 +55,24 @@
             echo "</div>";
         }
     }
-    ?>
+?>-->
+
+<div class="container">
+      <div class="grid-item">Item 1</div>
+      <div class="grid-item">Item 2</div>
+      <div class="grid-item">Item 3</div>  
+      <div class="grid-item">Item 4</div>
+      <div class="grid-item">Item 5</div>
+      <div class="grid-item">Item 6</div>  
+      <div class="grid-item">Item 7</div>
+      <div class="grid-item">Item 8</div>
+      <div class="grid-item">Item 9</div>  
+  </div>
+
+</div>
 </body>
+<footer>
+    <h1>Shopping App Inc.</h1>
+</footer>
 
 </html>
